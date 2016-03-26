ad95031cb65456084045cb7a62593fc72e443e02
Paul Brown <paul90brown@gmail.com>
2016-03-26 18:31:52 -0500

    add flask example

---
+++ examples/flask/README.rst
@@ -0,0 +1,21 @@
+WTForms-SQLAlchemy + Flask examples.
+
+To run this example:
+
+1. Clone the repository::
+
+    git clone https://github.com/wtforms/wtforms-sqlalchemy.git
+    cd wtforms-sqlalchemy
+
+2. Create and activate a virtual environment::
+
+    virtualenv env
+    source env/bin/activate
+
+3. Install requirements::
+
+    pip install -r 'examples/flask/requirements.txt'
+
+4. Run the application::
+
+    python examples/flask/basic.py
---
+++ examples/flask/basic.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+from wtforms_sqlalchemy.orm import model_form
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sample_db.sqlite'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+
+class Car(db.Model):
+    __tablename__ = 'cars'
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(50))
+    model = db.Column(db.String(50))
+
+    def __unicode__(self):
+        return self.name
+
+
+CarForm = model_form(Car)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def create_car():
+    car = Car()
+    success = False
+
+    if request.method == 'POST':
+        form = CarForm(request.form, obj=car)
+        if form.validate():
+            form.populate_obj(car)
+            db.session.add(car)
+            db.session.commit()
+            success = True
+    else:
+        form = CarForm(obj=car)
+
+    return render_template('create.html', form=form, success=success)
+
+
+if __name__ == '__main__':
+    db.create_all()
+
+    app.run(debug=True)
