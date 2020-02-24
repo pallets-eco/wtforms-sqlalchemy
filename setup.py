a4cf3e6454ade53c613e9bc46ba09239a7dbd6e7
Michael Lenzen <m.lenzen@gmail.com>
2020-02-23 23:37:33 -0600

    Update Python version classifiers
    Also, add tests_require to setup

--- setup.py
+++ setup.py
@@ -17,11 +17,12 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
@@ -30,6 +31,7 @@ setup(
     ],
     package_data={
     },
+    tests_require=['coverage'],
     test_suite='tests.tests',
     install_requires=['WTForms>=1.0.5', 'SQLAlchemy>=0.7.10'],
 )
